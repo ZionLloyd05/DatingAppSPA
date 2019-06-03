1b1f5fb4f1eab9b61e728552cd7c0e1b830754db
ZionLloyd05 <zionlloyd05@gmail.com>
2019-06-03 02:42:29 +0100

    routes setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { appRoutes } from "./routes";
+import { RouterModule } from "@angular/router";
 import { AlertifyService } from "./_services/alertify.service";
 import { AuthService } from "./_services/auth.service";
 import { BrowserModule } from "@angular/platform-browser";
@@ -13,6 +15,8 @@ import { NavComponent } from "./nav/nav.component";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { ListsComponent } from "./lists/lists.component";
 
 @NgModule({
   declarations: [
@@ -20,14 +24,17 @@ import { ErrorInterceptorProvider } from "./_services/error.interceptor";
     ValueComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
   bootstrap: [AppComponent]
