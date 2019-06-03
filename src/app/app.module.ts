2992fdcc0dd08c015b19861a49995d329afd4042
ZionLloyd05 <zionlloyd05@gmail.com>
2019-06-03 02:26:10 +0100

    auth functionality and general  cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,35 @@
+import { AlertifyService } from "./_services/alertify.service";
 import { AuthService } from "./_services/auth.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from "ngx-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
 import { NavComponent } from "./nav/nav.component";
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      HttpClientModule,
-      FormsModule
-   ],
-   providers: [
-      AuthService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+  ],
+  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
